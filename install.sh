6af04686787f5e968e0202b2e0de7ace10fdc759
ianbogda <ianbogda@gmail.com>
2015-04-23 14:37:42 +0200

    * update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,12 @@ apt-get -qq --assume-yes install curl git fail2ban > /dev/null
 if [[ ! -e /opt/fail2ban/installed ]]; then
 
     git clone https://github.com/cdg46/fail2ban.git /opt/fail2ban
-    cd /opt/fail2ban
+    cd /opt/fail2ban/config
 
     # Install in system
     cp -r filter.d/*.conf /etc/fail2ban/filter.d/
-    cp jail.local /etc/fail2ban/jail.local
+    cp *.local /etc/fail2ban/
+    cp *.conf /etc/fail2ban/
 
     touch /opt/fail2ban/installed
 fi
