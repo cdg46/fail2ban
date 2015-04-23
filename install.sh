f5fe129dce5e4df74437d3e5d09037fea08dbd77
ianbogda <ianbogda@gmail.com>
2015-04-23 16:43:26 +0200

    + cunstomize install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+INSTALL=0
 # Require git
+echo "Install curl git fail2ban if necessary."
 apt-get -qq --assume-yes install curl git fail2ban > /dev/null
 
 if [[ ! -e /opt/fail2ban/installed ]]; then
-
+    INSTALL=1
+    echo "Installing fail2ban post install"
     git clone https://github.com/cdg46/fail2ban.git /opt/fail2ban
     touch /opt/fail2ban/installed
 
@@ -13,11 +15,22 @@ if [[ ! -e /opt/fail2ban/installed ]]; then
         git pull origin master
 fi
 
+read -p "Email's receiver : " destemail
+read -p "Structure  : " structure
+
 # Install in system
 cd /opt/fail2ban/config
 cp -r filter.d/*.conf /etc/fail2ban/filter.d/
 cp *.local /etc/fail2ban/
 cp *.conf /etc/fail2ban/
 
-service fail2ban restart
+sed -i -e 's/root@localhost/$destemail/' /etc/fail2ban/jail.local
+sed -i -e 's/__STRUCTURE__/$structure/' /etc/fail2ban/jail.local
+sed -i -e 's/__VM__/$(hostname)/' /etc/fail2ban/jail.local
+
+if [[ $INSTALL == 1 ]] ; then
+    service fail2ban start
+else
+    service fail2ban restart
+fi
 #done
