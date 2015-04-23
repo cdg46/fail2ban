dd5ec264c9909a16f835d2347081f213e7a595c3
ianbogda <ianbogda@ubuntu.ring.adds>
2015-04-23 13:47:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#
+
+## install
+  curl -sSL https://raw.githubusercontent.com/cdg46/fail2ban/master/install.sh | bash
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Require git
+apt-get -qq --assume-yes install curl git fail2ban > /dev/null
+
+if [[ ! -e /opt/fail2ban/installed ]]; then
+
+    git clone https://github.com/cdg46/fail2ban.git /opt/fail2ban
+    cd /opt/fail2ban
+
+    # Install in system
+    cp -r filter.di/*.conf /etc/fail2ban/filter.d/
+    cp jail.local /etc/fail2ban/jail.local
+
+    touch /opt/fal2ban/installed
+fi
+#done
