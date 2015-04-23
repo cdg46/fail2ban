409943570bad5377057fcf16da78bb63253cd593
ianbogda <ianbogda@gmail.com>
2015-04-23 17:20:54 +0200

    * mail-whois

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ read -p "Structure  : " structure
 # Install in system
 cd /opt/fail2ban/config
 cp -r filter.d/*.conf /etc/fail2ban/filter.d/
+cp -r action.d/*.conf /etc/fail2ban/action.d/
 cp *.local /etc/fail2ban/
 cp *.conf /etc/fail2ban/
 
